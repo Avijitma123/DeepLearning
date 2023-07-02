117c7d0c4cf945b64606530e015e11c11e577089
Avijitma123 <avijitdeybira@gmail.com>
2023-07-03 01:49:30 +0530

    Transformer

--- Transformer/Transformer.py
+++ Transformer/Transformer.py
@@ -99,6 +99,6 @@ class Transformer(nn.Module):
         src_mask = self.make_src_mask(src)
         trg_mask = self.make_trg_mask(trg)
         enc_src = self.encoder(src, src_mask)
-        out = self.decoder(trg, enc_src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask,trg_mask)
 
         return out
